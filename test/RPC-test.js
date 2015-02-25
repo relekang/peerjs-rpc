9ca0cf4d8d91f1306c33a0d611c97502b718c551
Rolf Erik Lekang <me@rolflekang.com>
2015-02-25 19:24:45 +0100

    Support promises with bluebird

--- test/RPC-test.js
+++ test/RPC-test.js
@@ -32,66 +32,106 @@ describe('RPC', function() {
         n2 = new RPC('n2', scope);
     });
 
-    it('should ping and receive pong', function(done) {
-        n1.ping('n2', function(result) {
-            expect(result).to.be.truthy;
-            done();
+    describe('using callbacks', function() {
+        it('should ping and receive pong', function(done) {
+            n1.ping('n2', function(result) {
+                expect(result).to.be.truthy;
+                done();
+            });
+        });
+
+        it('should invoke with no arguments and return value', function(done) {
+            n1.invoke('n2', 'pinger', undefined, function(err, result) {
+                if (err) {
+                    return done(err)
+                }
+
+                expect(result).to.equal('pong');
+                done();
+            });
+        });
+
+        it('should invoke with one argument and return value', function(done) {
+            n1.invoke('n2', 'ping', '42', function(err, result) {
+                if (err) {
+                    return done(err)
+                }
+
+                expect(result).to.equal('pong: 42');
+                done();
+            });
+        });
+
+        it('should invoke with multiple arguments and return value', function(done) {
+            n1.invoke('n2', 'add', [40, 2], function(err, result) {
+                if (err) {
+                    return done(err)
+                }
+
+                expect(result).to.equal(42);
+                done();
+            });
+        });
+
+        it('should be able to reference scope in invoked functions', function(done) {
+            n1.invoke('n2', 'getAnswer', undefined, function(err, result) {
+                if (err) {
+                    return done(err)
+                }
+
+                expect(result).to.equal(42);
+                done();
+            });
+        });
+
+
+        it('should return attribute value', function(done) {
+            n1.attr('n2', 'answer', function(err, result) {
+                if (err) {
+                    return done(err)
+                }
+
+                expect(result).to.equal(42);
+                done();
+            });
         });
     });
 
-    it('should invoke with no arguments and return value', function(done) {
-        n1.invoke('n2', 'pinger', undefined, function(err, result) {
-            if (err) {
-                return done(err)
-            }
-
-            expect(result).to.equal('pong');
-            done();
+    describe('using promises', function() {
+        it('should ping and receive pong', function() {
+            n1.ping('n2').then(function(result) {
+                expect(result).to.be.truthy;
+            });
         });
-    });
 
-    it('should invoke with one argument and return value', function(done) {
-        n1.invoke('n2', 'ping', '42', function(err, result) {
-            if (err) {
-                return done(err)
-            }
-
-            expect(result).to.equal('pong: 42');
-            done();
+        it('should invoke with no arguments and return value', function() {
+            n1.invoke('n2', 'pinger', undefined).then(function(result) {
+                expect(result).to.equal('pong');
+            });
         });
-    });
 
-    it('should invoke with multiple arguments and return value', function(done) {
-        n1.invoke('n2', 'add', [40, 2], function(err, result) {
-            if (err) {
-                return done(err)
-            }
-
-            expect(result).to.equal(42);
-            done();
+        it('should invoke with one argument and return value', function() {
+            n1.invoke('n2', 'ping', '42').then(function(result) {
+                expect(result).to.equal('pong: 42');
+            });
         });
-    });
 
-    it('should be able to reference scope in invoked functions', function(done) {
-        n1.invoke('n2', 'getAnswer', undefined, function(err, result) {
-            if (err) {
-                return done(err)
-            }
-
-            expect(result).to.equal(42);
-            done();
+        it('should invoke with multiple arguments and return value', function() {
+            n1.invoke('n2', 'add', [40, 2]).then(function(result) {
+                expect(result).to.equal(42);
+            });
         });
-    });
 
+        it('should be able to reference scope in invoked functions', function() {
+            n1.invoke('n2', 'getAnswer', undefined).then(function(result) {
+                expect(result).to.equal(42);
+            });
+        });
 
-    it('should return attribute value', function(done) {
-        n1.attr('n2', 'answer', function(err, result) {
-            if (err) {
-                return done(err)
-            }
-
-            expect(result).to.equal(42);
-            done();
+        it('should return attribute value', function() {
+            n1.attr('n2', 'answer').then(function(result) {
+                expect(result).to.equal(42);
+            });
         });
     });
 });
